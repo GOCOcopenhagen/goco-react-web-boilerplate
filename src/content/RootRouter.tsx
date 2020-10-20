4223ac2985b65aeaa77d551047df8f47480d69d4
Anton Tobias Jensen <41019970+Statsministeriet@users.noreply.github.com>
2020-10-20 18:11:52 +0200

    Update RootRouter.tsx

--- src/content/RootRouter.tsx
+++ src/content/RootRouter.tsx
@@ -14,7 +14,7 @@ export default function RootRouter() {
             {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
             <Switch>
-                <Route path="*">
+                <Route exact path="*">
                     <IndexPage />
                 </Route>
             </Switch>
